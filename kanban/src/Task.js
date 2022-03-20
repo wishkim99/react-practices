053a79afca43b95269569398f3c4f3bcd723632f
wishkim99 <zzsowon@naver.com>
2022-03-21 08:32:57 +0900

    task check 하기

--- kanban/src/Task.js
+++ kanban/src/Task.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
-import styles from "./assets/css/Task.css";
-//배열을 상태에다 넣기
+import React, {useState} from 'react'
+import styles from './assets/css/Task.css';
 
-const Task = ({ name, done }) => {
-  const [check, setCheck] = useState(done);
+const Task = ({name, done}) => {
+  const [stateDone, setStateDone] = useState(done);
 
-  const onChangeInputCheck = function (e) {
-    const status = e.target.value === done ? "yes" : "no";
-  };
   return (
     <li className={styles.TaskList__Task}>
-      <input
-        type="checkbox"
-        value={status}
-        checked={check === "yes"}
-        onChange={onChangeInputCheck}
-      />
-      {name}
-      <a href="#" className={styles.TaskList__Task__remove}></a>
+        <input
+          type='checkbox'
+          checked={stateDone}
+          onClick={e => setStateDone(!stateDone)} />
+        {name}
+        <a href='#' className={styles.TaskList__Task__remove}></a>
     </li>
   );
-};
+}
 
 export default Task;
