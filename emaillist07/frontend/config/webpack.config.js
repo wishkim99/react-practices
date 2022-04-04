2f1c6c0de8bf68252677a07f47d726a44b044f09
wishkim99 <zzsowon@naver.com>
2022-04-04 14:19:34 +0900

    profiling

--- emaillist07/frontend/config/webpack.config.js
+++ emaillist07/frontend/config/webpack.config.js
@@ -1,43 +1,44 @@
 const path = require('path');
 
-module.exports = (env) => ({
-    mode: "none",
-    entry: path.resolve('src/index.js'),
-    output: {
-        path: path.resolve('../backend/src/main/resources'),
-        filename: 'static/js/main.js',
-        assetModuleFilename: 'static/images/[hash][ext]'
-    },
-    module: {
-        rules: [{
-            test: /\.(png|gif|jpe?g|svg|ico|tiff?|bmp)$/i,
-            type: 'asset/resource'
-        }, {
-            test: /\.(sa|sc|c)ss$/i,
-            use: [
-                'style-loader',
-                {loader: 'css-loader', options: { modules: true }},
-                'sass-loader'
-            ]
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-                configFile: path.resolve('config/babel.config.json')
-            }
-        }]
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        host: "0.0.0.0",
-        port: 9090,
-        proxy: {
-            '/api': 'http://localhost:8080'
+module.exports = function(env) { 
+    return {
+        entry: path.resolve(`src/index.js`),
+        output: {
+            path: path.resolve('../backend/src/main/resources'),
+            filename: 'static/js/main.js',
+            assetModuleFilename: 'static/images/[hash][ext]'
         },
-        liveReload: true,
-        hot: false,
-        compress: true,
-        historyApiFallback: true
-    }
-});
+        module: {
+            rules:[{
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    configFile: path.resolve('config/babel.config.json')
+                }
+            }, {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    'style-loader',
+                    {loader: 'css-loader', options: {modules: true}},
+                    'sass-loader'
+                ]
+            }, {
+                test: /\.(png|git|jpe?g|svg|ico|tiff?|bmp)$/i,
+                type: 'asset/resource'
+            }]
+        },
+        devtool: "eval-source-map",
+        devServer: {
+            host: '0.0.0.0',
+            port: 9090,
+            proxy: {
+                '/api': 'http://localhost:8080'
+            },
+            liveReload: true,
+            hot: false,
+            compress: true,
+            historyApiFallback: true
+        } 
+    };
+}
