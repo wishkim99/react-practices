7c3b0ba3b3ab40bd788bc25c9834f2c716a2469b
wishkim99 <zzsowon@naver.com>
2022-04-06 20:12:09 +0900

    모바일버전

--- test/src/Sidebar.js
+++ test/src/Sidebar.js
@@ -4,16 +4,21 @@ import {
   CDBSidebarFooter,
   CDBSidebarHeader,
   CDBSidebarMenu,
-  CDBSidebarMenuItem
+  CDBSidebarMenuItem,
+  
 } from 'cdbreact';
+
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { DropdownButton, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+//import './assets/css/styles.css';
 
 const Sidebar = () => {
   return (
     <div
       style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
     >
+      
       <CDBSidebar textColor="#fff" backgroundColor="#333">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
           <a
@@ -27,69 +32,64 @@ const Sidebar = () => {
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-          <Nav.Link href="#pricing"></Nav.Link>								
-          <CDBSidebarMenuItem icon="columns">
-           <NavDropdown title="기준정보 관리" id="collasible-nav-dropdown">	
-            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>				
-            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>			
-          </NavDropdown>
-          </CDBSidebarMenuItem>	
-
-          <CDBSidebarMenuItem icon="table">
-           <NavDropdown title="평가계획 관리" id="collasible-nav-dropdown">	
-            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>				
-            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>			
-          </NavDropdown>
-          </CDBSidebarMenuItem>	
-
-          <CDBSidebarMenuItem icon="exclamation-circle">
-           <NavDropdown title="시스템 관리" id="collasible-nav-dropdown">	
-            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>				
-            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>			
-          </NavDropdown>
-          </CDBSidebarMenuItem>	
-
-          <CDBSidebarMenuItem icon="user">
-           <NavDropdown title="보고서 관리" id="collasible-nav-dropdown">	
-            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>				
-            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>			
-          </NavDropdown>
-          </CDBSidebarMenuItem>	
-
-          <CDBSidebarMenuItem icon="chart-line">
-           <NavDropdown title="시스템 관리" id="collasible-nav-dropdown">	
-            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>				
-            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>			
-            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>			
-          </NavDropdown>
-          </CDBSidebarMenuItem>	
+                    
     
 
-   
+          <CDBSidebarMenuItem icon="columns">
+           <NavDropdown title="기준정보 관리" id="collasible-nav-dropdown"> 
+                <NavDropdown.Item href="#action/3.1">포스코ICT</NavDropdown.Item>                  
+                <NavDropdown.Item>포스코 인터네셔널</NavDropdown.Item>                                  
+          </NavDropdown>
+          </CDBSidebarMenuItem> 
+
+          <CDBSidebarMenuItem icon="table">
+           <NavDropdown title="평가계획 관리" id="collasible-nav-dropdown"> 
+            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>        
+            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>     
+          </NavDropdown>
+          </CDBSidebarMenuItem> 
+
+          <CDBSidebarMenuItem icon="exclamation-circle">
+           <NavDropdown title="시스템 관리" id="collasible-nav-dropdown">  
+            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>        
+            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>     
+          </NavDropdown>
+          </CDBSidebarMenuItem> 
+
+          <CDBSidebarMenuItem icon="user">
+           <NavDropdown title="보고서 관리" id="collasible-nav-dropdown">  
+            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>        
+            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>     
+          </NavDropdown>
+          </CDBSidebarMenuItem> 
+
+          <CDBSidebarMenuItem icon="chart-line">
+           <NavDropdown title=" 관리" id="collasible-nav-dropdown"> 
+            <CDBSidebarMenuItem icon="table">담당 통제 현황</CDBSidebarMenuItem>        
+            <CDBSidebarMenuItem icon="table">공지사항</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">자료실</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">RCM 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">담당자 관리</CDBSidebarMenuItem>      
+            <CDBSidebarMenuItem icon="table">IT 시스템 담당자 관리</CDBSidebarMenuItem>     
+            <CDBSidebarMenuItem icon="table">모집단수/샘플수 관리</CDBSidebarMenuItem>     
+          </NavDropdown>
+          </CDBSidebarMenuItem> 
+
           </CDBSidebarMenu>
         </CDBSidebarContent>
 
