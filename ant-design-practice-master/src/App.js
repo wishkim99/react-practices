a149dc384bb89f1eedb6c088c649d0480b242206
wishkim99 <zzsowon@naver.com>
2022-04-08 15:11:51 +0900

    loginForm 구현

--- ant-design-practice-master/src/App.js
+++ ant-design-practice-master/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { useRoutes } from "react-router";
 
 import Main from "./components/Main";
+import Login from "./components/Login";
 import MyLayout from "./components/MyLayout";
 
-import "./assets/css/App.css";
 
 export default function App() {
   return useRoutes([
     { path: "layout", element: <MyLayout /> },
     { path: "*", element: <Main /> },
+   { path: "login", element: <Login /> }
   ]);
+
 }
--- ant-design-practice-master/src/index.js
+++ ant-design-practice-master/src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import {render} from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-
+import "./assets/css/App.css";
 render(
     <Router>
         <App/>
